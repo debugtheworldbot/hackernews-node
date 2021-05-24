4aa9144af9d5facdc9420369a6a6f4e49e1fe34a
tian <istiancz@gmail.com>
2021-05-24 10:47:04 +0800

    query links

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ const {ApolloServer} = require('apollo-server')
 const typeDefs = `
   type Query{
     info:String!
+    feed:[Link!]!
+  }
+  type Link{
+    id:ID!
+    description:String!
+    url:String!
   }
 `
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
 const resolvers = {
   Query: {
-    info: () => 'this is the api of hackerNews!'
+    info: () => 'this is the api of hackerNews!',
+    feed: () => links
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
@@ -17,4 +34,4 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen().then(({url})=>console.log(`server running on ${url}`))
+server.listen().then(({url}) => console.log(`server running on ${url}`))

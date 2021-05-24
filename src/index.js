9864630bba0a47abff09c1039dd2290b20f11cc0
tian <istiancz@gmail.com>
2021-05-24 14:48:18 +0800

    post links by mutation

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
 const {ApolloServer} = require('apollo-server')
 
-const typeDefs = `
-  type Query{
-    info:String!
-    feed:[Link!]!
-  }
-  type Link{
-    id:ID!
-    description:String!
-    url:String!
-  }
-`
-
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
+let idCount = links.length
 const resolvers = {
   Query: {
     info: () => 'this is the api of hackerNews!',
     feed: () => links
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation:{
+    post:(parent,args)=>{
+      const link = {
+        id:`link-${idCount++}`,
+        description:args.description,
+        url:args.url
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
+const fs = require('fs')
+const path = require('path')
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'),"utf-8"),
   resolvers,
 })
 

40dbaa19f29fa2a3a9876c306bffecec0d41022e
tian <istiancz@gmail.com>
2021-05-28 11:24:11 +0800

    filter feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,12 @@
-const feed = (parent, args, context) => context.prisma.link.findMany()
+const feed = (parent, args, context) => {
+  const where = args.filter ? {
+    OR: [
+      {description: {contains: args.filter}},
+      {url: {contains: args.filter}},
+    ]
+  } : {}
+  return context.prisma.link.findMany({where})
+}
 
 const link = (parent, args, context) => context.prisma.link.findFirst({
   where: {

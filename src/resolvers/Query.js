97464f84bfb4121d351f79293fc22938e53330dd
tian <istiancz@gmail.com>
2021-05-28 14:47:47 +0800

    show count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,17 +1,22 @@
 const feed = (parent, args, context) => {
-  const {skip,take,orderBy} = args
+  const {skip, take, orderBy} = args
   const where = args.filter ? {
     OR: [
       {description: {contains: args.filter}},
       {url: {contains: args.filter}},
     ]
   } : {}
-  return context.prisma.link.findMany({
+  const links = context.prisma.link.findMany({
     where,
     skip,
     take,
     orderBy
   })
+  const count = context.prisma.link.count({where})
+  return {
+    links,
+    count
+  }
 }
 
 const link = (parent, args, context) => context.prisma.link.findFirst({
